dd0a8dceb2369cdc199467e94cc2589374c3286b
kuhomeprojects <kuhomeprojects@gmail.com>
2019-03-01 15:57:52 +0700

    [Update] - Initial envronment to start server

---
+++ index.js
@@ -0,0 +1,43 @@
+import express from "express";
+import bodyParser from "body-parser";
+import con1 from './controller/controller1.js';
+import * as config from './config/serverconfig'
+
+const app = express();
+app.use(bodyParser({
+    limit: '50mb'
+}));
+app.use(bodyParser.json()); // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
+    extended: true
+}));
+app.all('*', function (req, res, next) {
+    /**
+     * Response settings
+     * @type {Object}
+     */
+    const responseSettings = {
+        "AccessControlAllowOrigin": req.headers.origin,
+        "AccessControlAllowHeaders": "Content-Type,X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5,  Date, X-Api-Version, X-File-Name",
+        "AccessControlAllowMethods": "POST, GET, PUT, DELETE, OPTIONS",
+        "AccessControlAllowCredentials": true
+    };
+
+    /**
+     * Headers
+     */
+    res.header("Access-Control-Allow-Credentials", responseSettings.AccessControlAllowCredentials);
+    res.header("Access-Control-Allow-Origin", responseSettings.AccessControlAllowOrigin);
+    res.header("Access-Control-Allow-Headers", (req.headers['access-control-request-headers']) ? req.headers['access-control-request-headers'] : "x-requested-with");
+    res.header("Access-Control-Allow-Methods", (req.headers['access-control-request-method']) ? req.headers['access-control-request-method'] : responseSettings.AccessControlAllowMethods);
+    if ('OPTIONS' == req.method) {
+        res.send(200);
+    } else {
+        next();
+    }
+});
+app.use('/test', con1);
+app.listen(config.port, () => {
+    console.log('Start server at port ' + config.port)
+});
+
