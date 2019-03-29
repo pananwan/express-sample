96484c8340a5a1dfc42a1a24059c158a7da4f406
dasxeriez <37256871+dasxeriez@users.noreply.github.com>
2019-03-29 14:31:46 +0700

    Update controller1.js

--- controller/controller1.js
+++ controller/controller1.js
@@ -1,56 +1,72 @@
 import express from "express";
-import mongoist from "mongoist"
-import mongojs from "mongojs"
+import mongoist from "mongoist";
+import mongojs from "mongojs";
 
 const router = express.Router();
 //import pool from "../config/dbpool";
 // import mongojs from "../config/mongodb";
-const databaseUrl = 'mongodb://angularku1:angularku1@ds061464.mlab.com:61464/mynote';
+const databaseUrl =
+  "mongodb://angularku1:angularku1@ds061464.mlab.com:61464/mynote";
 const mongojsDb = mongojs(databaseUrl);
 const db = mongoist(mongojsDb);
 const db2 = mongojs.connect;
 
-router.get("/", async (req, res) => {
-    /* let data = await db2.user_detail.find({id: "5721602287"}, function (err, docs) {
-          res.send(docs);
-      });*/
-    let data = await db.user_detail.find({id: "5721602287"});
-    res.send(data)
+router.get("/getDetail", async (req, res) => {
+  let userDetail = await db.user_detail.find({ id: req.body.id });
+  let noteDetail = await db.note_detail.find({ id: req.body.id });
+  res.send({ user: userDetail, noteDetail: noteDetail });
 });
-
+/*
+    Request
+* id : "5721602287"
+* name : "student name"
+* */
 router.post("/createProfile", async (req, res) => {
+  let data = await db.user_detail.find({ id: req.body.id });
+  if (data.length > 0) {
+    res.send(
+      data.map(m => {
+        let v = m;
+        v.isRegis = true;
+        return v;
+      })
+    );
+  } else {
     let user = {
-        id: req.body.id,
-        name: req.body.name
-    }
+      id: req.body.id,
+      name: req.body.name
+    };
     let note_type = [
-        {
-            "id": 1,
-            "name": "ดนตรี",
-            "list": [{
-                "id": 1,
-                "detail": "ข้อมูลทดสอบ",
-                "priority": 10
-            }]
-        },
-        {
-            "id": 2,
-            "name": "การเรียน",
-            "list": []
-        },
-        {
-            "id": 3,
-            "name": "ท่องเที่ยว",
-            "list": []
-        }
+      {
+        id: 1,
+        name: "ดนตรี",
+        list: [
+          {
+            id: 1,
+            detail: "ข้อมูลทดสอบ",
+            priority: 10
+          }
+        ]
+      },
+      {
+        id: 2,
+        name: "การเรียน",
+        list: []
+      },
+      {
+        id: 3,
+        name: "ท่องเที่ยว",
+        list: []
+      }
     ];
     let obj = {
-        id: user.id,
-        note_type: note_type
-    }
+      id: user.id,
+      note_type: note_type
+    };
     let result = await db.user_detail.insert(user);
     let result2 = await db.note_detail.insert(obj);
-    res.send({info: result, info2: result2})
+    res.send({ info: result, info2: result2 });
+  }
 });
 
 /*
@@ -60,15 +76,15 @@ router.post("/createProfile", async (req, res) => {
 * name : "new name"
 * */
 router.post("/updateNoteName", async (req, res) => {
-    let info = await db.note_detail.update(
-        {id: req.body.id, note_type: {$elemMatch: {noteid: req.body.noteId}}},
-        {$set: {'note_type.$.name': req.body.name}}
-    );
-    if (info.nModified == 1) {
-        res.send({success: true});
-    } else {
-        res.send({success: false});
-    }
+  let info = await db.note_detail.update(
+    { id: req.body.id, note_type: { $elemMatch: { noteid: req.body.noteId } } },
+    { $set: { "note_type.$.name": req.body.name } }
+  );
+  if (info.nModified == 1) {
+    res.send({ success: true });
+  } else {
+    res.send({ success: false });
+  }
 });
 
 /*
@@ -77,54 +93,47 @@ router.post("/updateNoteName", async (req, res) => {
 * noteId : 1
 * listId : 1
 * listDetail : "detail"
+* priority : 12
 * */
-
 router.post("/updateNoteDetail", async (req, res) => {
-    /*console.log(req.body);
-    let info = await db.note_detail.find();
-    console.log(info);*/
-    let detail = {
-        id: req.body.listId,
-        detail: req.body.detail,
-        priority: req.body.priority,
-    };
-
-    let info = await db.note_detail.update(
-        {
-            id: req.body.id,
-            note_type: {$elemMatch: {noteid: req.body.noteId}},
-            //'note_type.$.list': {$elemMatch: {id: req.body.listId,$elemMatch:{}}}
-        },
-        {$set: {'note_type.$.list.$[j].detail': 'Tests'}},
-        //{$set: {'note_type.$.list.$[i]': detail}},
-        {arrayFilters: [{'j.id': req.body.listId}]}
-    );
-
-    res.send(info);
-    // let result = await
+  let detail = {
+    id: req.body.listId,
+    detail: req.body.listDetail,
+    priority: req.body.priority
+  };
+  let info = await db.note_detail.update(
+    {
+      id: req.body.id,
+      "note_type.id": req.body.noteId,
+      "note_type.list.id": req.body.listId
+    },
+    {
+      $set: { "note_type.0.list.$": detail }
+    }
+  );
+  res.send(info);
 });
 
-//db.coll.update({}, {$set: {“a.$[i].c.$[j].d”: 2}}, {arrayFilters: [{“i.b”: 0}, {“j.d”: 0}]})
-let Input = {
-    a:
-        [
-            {
-                b: 0,
-                c:
-                    [
-                        {d: 0},
-                        {d: 1}]
-            },
-            {
-                b: 1, c:
-                    [{d: 0},
-                        {d: 1}]
-            }]
-}
+/*
+    Request
+* id : "5721602287"
+* noteId : 1
+* noteName : "noteName"
+* */
 router.post("/insertNote", async (req, res) => {
-    let result = await db.note_detail.update({id: req.body.id + ''},
-        {$push: {'note_type': {noteid: req.body.noteId, name: req.body.noteName, list: []}}});
-    res.send(result[0])
+  let result = await db.note_detail.update(
+    { id: req.body.id + "" },
+    {
+      $push: {
+        note_type: {
+          noteid: req.body.noteId,
+          name: req.body.noteName,
+          list: []
+        }
+      }
+    }
+  );
+  res.send(result[0]);
 });
 
 module.exports = router;
